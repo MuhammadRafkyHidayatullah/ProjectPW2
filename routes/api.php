493d892519187b0ae1b28cfd685c90dad270b88b
MuhammadRafkyHidayatullah <146694847+MuhammadRafkyHidayatullah@users.noreply.github.com>
2024-09-12 15:06:01 +0700

    PW2 12/9/2024
    Need To Make And Get And Post For Mahasiswa (Note To Self: How The Hell Make Controller With all The Function)

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,4 @@ Route::get('/user', function (Request $request) {
 Route::get('/fakultas',[FakultasController::class, 'index']);
 Route::get('/prodi',[ProdiController::class, 'index']);
 Route::post('/fakultas', [FakultasController::class, 'store']);
+Route::post('/prodi', [ProdiController::class, 'store']);
